6d1502ec111b87108de98dd445bf41b59be7d209
josiah perisho <863370@cognizant.com>
2025-09-20 22:10:28 -0500

    okay, added ser/deser for real this time.

--- levels/multiplayer_V/test/orbits/network_sync.gd
+++ levels/multiplayer_V/test/orbits/network_sync.gd
@@ -9,21 +9,28 @@ var network_id : int
 
 @export var synced_vars : Array[PropertySelection]
   
-  
-func get_update():
+# TODO test this...maybe make a mirror scene. One object is controlled, the other just mirrors  
+
+## An update is composed of a 2 level Dictionary
+## { NodePath : { PropertyName : PropertyValue } }
+## So, a list of all the affected nodes, and each contains a 
+## map of their properties to values (Basically a simplified scene tree)
+func create_update() -> Dictionary[NodePath , Dictionary]:
   var scene_updates = {}
   # go through each tracked node
   for selection in synced_vars:
     # Get the properties and values for each node
     var prop_updates = {}
     for prop in selection.props:
+      # Store all the values of all the properties
       var val = get_node(selection.node).get(prop)
       prop_updates[prop]= val
+    # Associate those properties back to the nodepath it belongs to
     scene_updates[selection.node] = prop_updates
   return scene_updates
   
-# TODO test this...maybe make a mirror scene. One object is controlled, the other just mirrors
-func apply_update(scene_updates : Dictionary):
+## Apply updates created with create_update() method
+func apply_update(scene_updates : Dictionary[NodePath,Dictionary]):
   for update_node_path in scene_updates.keys():
     # Get the properties that belong to this node
     var update_properties = scene_updates[update_node_path] as Dictionary
